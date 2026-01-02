60dff9db4fa721c97bd6db8fddbcd6ca274aa2b8
fajars97-cpu <fajars97@gmail.com>
2026-01-02 13:42:14 +0700

    penambahan lat lng

--- src/data/jejaring.js
+++ src/data/jejaring.js
@@ -8,6 +8,8 @@ export const jejaringList = [
     wilayah: "Jakarta Selatan",
     gmapsUrl: "https://maps.app.goo.gl/e7HkB5i5cnSLGz1R7",
     gmapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1982.755032936078!2d106.8242623!3d-6.3278868!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69edd9a7f506f5%3A0x1d33834c098c491c!2sJl.%20Raya%20Jagakarsa%2C%20Kec.%20Jagakarsa%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1767152715857!5m2!1sid!2sid",
+    lat: -6.32773,
+    lng: 106.82416,
 
     // STATUS & TIPE
     status: "Aktif",
@@ -57,6 +59,8 @@ export const jejaringList = [
     wilayah: "Jakarta Selatan",
     gmapsUrl: "https://maps.app.goo.gl/fniYQAfLyA32rgNp7",
     gmapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.496658722587!2d106.8090242!3d-6.3296334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69efd5d3815073%3A0x8f45bf399612989d!2sParaslogy%20Aesthetic%20%26%20Wellness%20Clinic!5e0!3m2!1sid!2sid!4v1767152860484!5m2!1sid!2sid",
+    lat: -6.32957,
+    lng: 106.80902,
 
     // STATUS & TIPE
     status: "Aktif",
@@ -106,6 +110,8 @@ export const jejaringList = [
     wilayah: "Jakarta Selatan",
     gmapsUrl: "https://maps.app.goo.gl/8hAvnAnSqkq7CYrM9",
     gmapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31723.973294347725!2d106.7909513!3d-6.329633!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed2ee0550311%3A0x9946e01e3de20d75!2sHappy%20Dental%20Clinic%20Aeon%20Mall%20Tanjung%20Barat!5e0!3m2!1sid!2sid!4v1767153035253!5m2!1sid!2sid",
+    lat: -6.29585,
+    lng: 106.83947,
 
     // STATUS & TIPE
     status: "Aktif",
